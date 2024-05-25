c638d182617cabb09eb9e3e930125b9d8a7a9bad
Teddy Yongo <teyddie@gmail.com>
2024-05-25 07:28:10 +0300

    Setting up Oath session

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const https = require("https");
 const path = require("path");
 const passport = require("passport");
 const { Strategy } = require("passport-google-oauth20");
+const cookieSession = require("cookie-session");
 
 const dotEnv = require("dotenv");
 
@@ -21,14 +22,15 @@ const app = express();
  * 3 Initialize the passport package
  */
 
-//OAUTH creadentials
+//OAUTH and Cookies creadentials
 const config = {
 	CLIENT_ID: process.env.CLIENT_ID,
 	CLIENT_SECRET: process.env.CLIENT_SECRET,
+	COOKIE_KEY_1: process.env.COOKIE_KEY_1,
+	COOKIE_KEY_2: process.env.COOKIE_KEY_2,
 };
 
 //Setting up the passport strategy
-//hooking the callback to the google callback endpoint
 
 const AUTH_OPTIONS = {
 	callbackURL: "/auth/google/callback",
@@ -37,18 +39,39 @@ const AUTH_OPTIONS = {
 };
 
 function verifyCallback(accessToken, refreshToken, profile, done) {
-	console.log("google Profile", profile);
-
 	done(null, profile);
 }
 
 passport.use(new Strategy(AUTH_OPTIONS, verifyCallback));
 
+//Serializing data(Saving session to cookie)
+
+passport.serializeUser((user, done) => {
+	done(null, user.id);
+});
+
+//Deserialize data (Read session from the cookie)
+
+passport.deserializeUser((id, done) => {
+	done(null, id);
+});
 //Sets up the headers
 app.use(helmet());
 
-//Sets up passport session
+//Setting Cookie session
+app.use(
+	cookieSession({
+		name: "session",
+		maxAge: 24 * 60 * 60 * 1000,
+		keys: [config.COOKIE_KEY_1, config.COOKIE_KEY_2],
+	})
+);
+
+//Setting passport middlewar e session
 app.use(passport.initialize());
+
+//Authenticate the session
+app.use(passport.session());
 
 //Restictions
 
@@ -72,17 +95,13 @@ app.get(
 
 app.get(
 	"/auth/google/callback",
-	passport.authenticate(
-		"google",
-		{
-			failureRedirect: "/failure",
-			successRedirect: "/",
-			session: false,
-		},
-		(req, res) => {
-			console.log("Google did their thing!");
-		}
-	)
+	passport.authenticate("google", {
+		failureRedirect: "/failure",
+		successRedirect: "/",
+	}),
+	(req, res) => {
+		console.log("We are in the home page!");
+	}
 );
 
 app.get("/auth/logout", (res, req) => {});
